68cca802776521e3be749331c429a9102f1d9fb5
HYMMA <71206865+HYMMA@users.noreply.github.com>
2021-06-18 00:26:56 +1000

    updated sample app

--- SampleApp/MathematicsSampleApp.cs
+++ SampleApp/MathematicsSampleApp.cs
@@ -9,15 +9,15 @@ namespace SampleApp
         static void Main(string[] args)
         {
             var p = new Point(1, 2, 3);
-            var v1 = new Vector(p, new Point(1,.5,3));
-            var v2 = new Vector(new Point(2, 1.5, 4));
+            var v1 = new Vector(p, new Point(1,4,4));
+            var v2 = new Vector(new Point(4, 4, 0));
             var v3 = new Vector(new Point(4, 4, 0));
             var v4 = new Vector(new Point(3.999, 3.999, 0));
             object objV3 = v3;
             Console.WriteLine(string.Format("point p in feet is {0:ft}",p));
             Console.WriteLine(string.Format("v1 in meter {0}", v1));
             Console.WriteLine(string.Format("v1 in milllimeters is {0:mm}",v1));
-            Console.WriteLine("v1 in milllimeters in Iranian is "+v1.ToString("mm",new CultureInfo("fa-IR")));
+            Console.WriteLine("v1 in milllimeters written in German is "+v1.ToString("mm",new CultureInfo("de-DE")));
             Console.WriteLine(string.Format("v1 in inches unit is {0:in}",v1));
             Console.WriteLine($"v2 is {v2}");
             Console.WriteLine($"v3 is {v3}");
@@ -28,9 +28,9 @@ namespace SampleApp
             Console.WriteLine($"v1 unit vector is {v1.GetUnitVector()}");
             Console.WriteLine($"v1 . v2 {v1.DotProductWith(v2)}");
             Console.WriteLine($"v1 X v2 {v1.CrossProductWith(v2)}");
-            Console.WriteLine($" v1 is v2 ? {v1 == v2}");
+            Console.WriteLine($" v2 is v3 ? {v2 == v3}");
             Console.WriteLine($"v2 is objV3 ? {v2.Equals(objV3)}");
-            Console.WriteLine($"v2 is almost equalt to v4 ? {v2.AlmostEquals(v4, 0.005)}");
+            Console.WriteLine($"v2 is almost equalt to v4 ? {v3.AlmostEquals(v4, 0.005)}");
             Console.ReadLine();
         }
     }
