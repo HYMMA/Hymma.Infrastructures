c1f9734bcb1081484c9f022ae623246c439a03ea
HYMMA <71206865+HYMMA@users.noreply.github.com>
2021-06-20 17:27:55 +1000

    merged from development

--- SampleApp/MathematicsSampleApp.cs
+++ SampleApp/MathematicsSampleApp.cs
@@ -1,4 +1,5 @@
 ﻿using Hymma.Mathematics;
+using Hymma.Units;
 using System;
 using System.Globalization;
 
@@ -8,17 +9,22 @@ namespace SampleApp
     {
         static void Main(string[] args)
         {
+            SampleForMathematics();
+            SampleForUnits();
+        }
+        private static void SampleForMathematics()
+        {
             var p = new Point(1, 2, 3);
-            var v1 = new Vector(p, new Point(1,4,4));
+            var v1 = new Vector(p, new Point(1, 4, 4));
             var v2 = new Vector(new Point(4, 4, 0));
             var v3 = new Vector(new Point(4, 4, 0));
             var v4 = new Vector(new Point(3.999, 3.999, 0));
             object objV3 = v3;
-            Console.WriteLine(string.Format("point p in feet is {0:ft}",p));
+            Console.WriteLine(string.Format("point p in feet is {0:ft}", p));
             Console.WriteLine(string.Format("v1 in meter {0}", v1));
-            Console.WriteLine(string.Format("v1 in milllimeters is {0:mm}",v1));
-            Console.WriteLine("v1 in milllimeters written in German is "+v1.ToString("mm",new CultureInfo("de-DE")));
-            Console.WriteLine(string.Format("v1 in inches unit is {0:in}",v1));
+            Console.WriteLine(string.Format("v1 in milllimeters is {0:mm}", v1));
+            Console.WriteLine("v1 in milllimeters written in German is " + v1.ToString("mm", new CultureInfo("de-DE")));
+            Console.WriteLine(string.Format("v1 in inches unit is {0:in}", v1));
             Console.WriteLine($"v2 is {v2}");
             Console.WriteLine($"v3 is {v3}");
             Console.WriteLine($"v4 is {v4}");
@@ -31,6 +37,18 @@ namespace SampleApp
             Console.WriteLine($" v2 is v3 ? {v2 == v3}");
             Console.WriteLine($"v2 is objV3 ? {v2.Equals(objV3)}");
             Console.WriteLine($"v2 is almost equalt to v4 ? {v3.AlmostEquals(v4, 0.005)}");
+            Console.ReadLine();
+        }
+        private static void SampleForUnits()
+        {
+            var mass = new Mass(37, MassUnit.Kg); //make a new mass struct
+            var vol = new Volume(20, VolUnit.cm3);//new voluem
+            var density = new Density(mass, vol);//construct density based on these objects
+            Console.WriteLine(density);
+
+            mass.Unit = new Gram(); //change the unit of the mass object
+            vol.Unit = new CubicMillimeter(); //change the unit of volume object
+            Console.WriteLine(density); //density value reflects updates in units
             Console.ReadLine();
         }
     }
