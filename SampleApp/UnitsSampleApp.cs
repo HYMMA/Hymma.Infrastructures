30d377f9ec99ba9792ecc9c6b42b27f44687c64b
HYMMA <71206865+HYMMA@users.noreply.github.com>
2021-06-20 17:20:19 +1000

    added a smapel of unit to sample app

---
+++ SampleApp/UnitsSampleApp.cs
@@ -0,0 +1,21 @@
+﻿using Hymma.Units;
+using System;
+
+namespace SampleApp
+{
+    public class UnitsSampleApp
+    {
+        static void Main(string[] args)
+        {
+            var mass = new Mass(37, MassUnit.Kg); //make a new mass struct
+            var vol = new Volume(20, VolUnit.cm3);//new voluem
+            var density = new Density(mass, vol);//construct density based on these objects
+            Console.WriteLine(density);
+
+            mass.Unit = new Gram(); //change the unit of the mass object
+            vol.Unit = new CubicMillimeter(); //change the unit of volume object
+            Console.WriteLine(density); //density value reflects updates in units
+            Console.ReadLine();
+        }
+    }
+}
