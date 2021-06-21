d8430bedfcb07b39ac1cb48d62cd4e4aa87f0c19
HYMMA <71206865+HYMMA@users.noreply.github.com>
2021-06-22 00:30:13 +1000

    removed using statements

--- Hymma.Units/Core/IUnitOfMass.cs
+++ Hymma.Units/Core/IUnitOfMass.cs
@@ -1,16 +1,10 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace Hymma.Units
+﻿namespace Hymma.Units
 {
     /// <summary>
     /// a unit of mass measurement (Kg, gr, oz ...)
     /// </summary>
     public interface IUnitOfMass : IUnitOfMeasure
     {
-        
+
     }
 }
